79c793710045f8b5edfb8c69094c6ce58381022f
Jon Lamb <lamb.jon.io@gmail.com>
2019-01-02 15:42:51 -0800

    Refactoring

--- src/attitude_controller.rs
+++ src/attitude_controller.rs
@@ -5,7 +5,9 @@ use nphysics3d::math::Force;
 use pid::Pid;
 
 pub struct AttitudeController {
-    pid_yaw: Pid<f32>,
+    //pid_rot_x: Pid<f32>,
+    pid_rot_y: Pid<f32>,
+    //pid_rot_z: Pid<f32>,
 }
 
 impl AttitudeController {
@@ -17,10 +19,10 @@ impl AttitudeController {
         let p_limit = TORQUE_LIMIT;
         let i_limit = 15.0;
         let d_limit = 10.0;
-        let mut pid_yaw = Pid::new(kp, ki, kd, p_limit, i_limit, d_limit);
-        pid_yaw.update_setpoint(0.0);
+        let mut pid_rot_y = Pid::new(kp, ki, kd, p_limit, i_limit, d_limit);
+        pid_rot_y.update_setpoint(0.0);
 
-        AttitudeController { pid_yaw }
+        AttitudeController { pid_rot_y }
     }
 
     pub fn update(
@@ -31,8 +33,8 @@ impl AttitudeController {
         let sp_rot = setpoint.scaled_axis();
         let input_rot = input.scaled_axis();
 
-        self.pid_yaw.update_setpoint(sp_rot.y);
-        let out_y = self.pid_yaw.next_control_output(input_rot.y).output;
+        self.pid_rot_y.update_setpoint(sp_rot.y);
+        let out_y = self.pid_rot_y.next_control_output(input_rot.y).output;
 
         Force::torque(Vector3::new(0.0, out_y, 0.0))
     }
